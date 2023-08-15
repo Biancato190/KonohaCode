847e756121647d2efef230ab852c740904eaad36
Biancato190 <140861049+Biancato190@users.noreply.github.com>
2023-08-14 22:43:48 -0300

    Add files via upload

---
+++ 03-Biancato.py
@@ -0,0 +1,7 @@
+
+valor1 = float(input("inserir o primeiro valor: "))
+valor2 = float(input("inserir o segundo valor: "))
+
+maior_valor = max(valor1, valor2)
+
+print(f"O maior valor é: {maior_valor}")
